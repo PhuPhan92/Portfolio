48c974c80058d5c94dff20b852764b1b34a284c4
Phan Phu <phanvietphu92@gmail.com>
2023-06-09 15:22:51 +0700

    temp

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: 16.463713, lng: 107.590866};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,6 +10,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Hue, Viet Nam' // Title Location
     });
 }
